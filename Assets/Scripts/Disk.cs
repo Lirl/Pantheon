51d83e93833957b350f364d3f86b252f71c6d426
GuyGinat <guyginat4@gmail.com>
2018-04-17 16:21:37 +0300

    Added Disk(Character defualt) + script, camera ratio fixer, placeholder background

---
+++ Assets/Scripts/Disk.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disk : MonoBehaviour {
+
+    private bool isMouseDown = false;
+    public Rigidbody Rigidbody;
+    public SpringJoint SpringJoint;
+    public float releaseTime = 0.15f;
+
+    private void Start() {
+        SpringJoint = GetComponent<SpringJoint>();
+        
+    }
+
+    private void Update() {
+        if (isMouseDown) {
+            Rigidbody.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void OnMouseDown() {
+        isMouseDown = true;
+        Rigidbody.isKinematic = true;
+    }
+
+    private void OnMouseUp() {
+        isMouseDown = false;
+        Rigidbody.isKinematic = false;
+        StartCoroutine(UnHook());
+    }
+
+    IEnumerator UnHook() {
+        yield return new WaitForSeconds(releaseTime);
+        Destroy(GetComponent<SpringJoint>());
+    }
+}
